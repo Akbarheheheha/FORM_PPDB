066f0a63cd17a6075240d41b627b5002dff3deae
AryaaaPPLG <ibu.masarya@gmail.com>
2026-01-07 10:51:45 +0700

    fix crud

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -30,7 +30,7 @@ class FormController extends Controller
             'no_telp' => $validated["no_telp"],
         ]);
 
-        return view('succesfully');
+        return redirect()->back()->with('success', 'Data berhasil dikirim. Terima kasih!');
     }
 
     public function export()
